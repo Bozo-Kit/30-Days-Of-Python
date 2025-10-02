4f71090c261f3375140d0bea14834326cac2a1d6
Bozo-Kit <spencerkitchen1999@gmail.com>
2025-10-02 11:02:03 -0400

    Day_3notes.py

---
+++ 03_Day_Operators/day_3.py
@@ -0,0 +1,32 @@
+#Day 3 of 30 days of python
+import math
+import cmath
+age = 26
+height = 6.33
+complex_number= 3 + 2j
+'''
+base = float(input("What is the measurement of the base in inches?: "))
+tri_height = float(input("What is the measurement of the height in inches?: "))
+tri_area = 0.5 * base * tri_height
+print(f"The area of your triangle is {round(tri_area)}")
+
+side_a = float(input("Enter the measurement for side a: "))
+side_b = float(input("Enter the measurement for side b: "))
+side_c = float(input("Enter the measurement for side c: "))
+
+perimeter = side_a + side_b + side_c
+print(f"The perimeter of your triangle is {round(perimeter, 2)}")
+'''
+rec_length = float(input("What is the length of your rectangle?: "))
+rec_width = float(input("What is the width of your rectangle?: "))
+rec_area = rec_length * rec_width
+
+print(f"The area of your rectangle is {rec_area}")
+
+
+
+
+
+
+
+                     
